3b7700ba753988b84a88e91c8d073f93cab05e97
jwfrasier <frasier.joseph@gmail.com>
2018-12-14 15:56:31 -0600

    we got the monster demons

--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -1,23 +1,49 @@
-import React from 'react'
-import PokemonCollection from './PokemonCollection'
-import PokemonForm from './PokemonForm'
-import { Search } from 'semantic-ui-react'
-import _ from 'lodash'
+import React from "react";
+import PokemonCollection from "./PokemonCollection";
+import PokemonForm from "./PokemonForm";
+import { Search } from "semantic-ui-react";
+import _ from "lodash";
+const URL = "http://localhost:3000/pokemon";
 
 class PokemonPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      pokemon: []
+    };
+  }
+
+  componentDidMount() {
+    this.fetchPokemon();
+  }
+
+  fetchPokemon = () => {
+    fetch(URL)
+      .then(res => res.json())
+      .then(pokemonCollection =>
+        this.setState({
+          pokemon: pokemonCollection
+        })
+      );
+  };
+
   render() {
+    console.log("this is the state of pokemon", this.state.pokemon);
     return (
       <div>
         <h1>Pokemon Searcher</h1>
         <br />
-        <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
+        <Search
+          onSearchChange={_.debounce(() => console.log("🤔"), 500)}
+          showNoResults={false}
+        />
         <br />
         <PokemonCollection />
         <br />
         <PokemonForm />
       </div>
-    )
+    );
   }
 }
 
-export default PokemonPage
+export default PokemonPage;
