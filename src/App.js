ae7266e86ef25a22d14dd536b61e05c5ce1e99b6
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2018-12-28 14:20:50 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react'
-import PokemonIndex from './components/PokemonIndex'
-import './App.css'
+import React from "react";
+import PokemonIndex from "./components/PokemonIndex";
+import "./App.css";
 
+// App Component is responsible for:
+// 1. Rendering PokemonIndex component
 const App = () => (
   <div className="App">
     <PokemonIndex />
   </div>
-)
+);
 
-export default App
+export default App;
